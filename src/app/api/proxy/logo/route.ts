1824c491574c063fe2d8a66c4e41c56160036308
Kulapichia <chkl@bu.edu>
2025-09-20 00:41:03 +0800

    Update route.ts

--- src/app/api/proxy/logo/route.ts
+++ src/app/api/proxy/logo/route.ts
@@ -29,7 +29,7 @@ export async function GET(request: Request) {
 
   const config = await getConfig();
   const liveSource = config.LiveConfig?.find((s: any) => s.key === source);
-  const ua = liveSource?.ua || 'AptvPlayer/1.4.10';
+  const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36';
 
   try {
     const decodedUrl = decodeURIComponent(imageUrl);
